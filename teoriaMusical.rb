f1d23716fa27bc25751ce3d94523db25fb4c2069
gustavorodi <37717445+gustavorodi@users.noreply.github.com>
2019-11-21 13:18:09 -0300

    Simplificando o código

--- teoriaMusical.rb
+++ teoriaMusical.rb
@@ -1,21 +1,24 @@
-class TeoriaMusical
+  class TeoriaMusical
     def initialize
         @notas = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
         @notaMaior = [0,2,2,1,2,2,2,1]
         @notaMenor = [0,2,1,2,2,1,2,2]
-        @arrayAnt  = []
-        @arrayPos  = []
         @newArray  = []
         @escalaMaior = []
         @escalaMenor = []
     end
 
-    def notasMaiores(nota)
-        localizacao = @notas.find_index(nota)
+    def iniciar
+        puts "Olá selecione uma nota músical das apresentadas a seguier:\n #{@notas}"
+        notaEscolida = gets.strip
+        puts "A sequência maior é #{notasMaiores(notaEscolida)}"
+        puts "A sequência menor é #{notasMenores(notaEscolida)}" 
+    end
 
-        @arrayAnt = @notas[0, localizacao]
-        @arrayPos = @notas[localizacao, @notas.size]
-        @newArray = @arrayPos + @arrayAnt
+    def notasMaiores(nota)
+
+        localizacao = @notas.find_index(nota)
+        reorganizarArray(localizacao)
 
         temp = 0
         for x in 0...7 do
@@ -23,15 +26,13 @@ class TeoriaMusical
             @escalaMaior.push(@newArray[temp])
         end
 
-        p @escalaMaior
+        return @escalaMaior
     end
     
     def notasMenores(nota)
-        localizacao = @notas.find_index(nota)
 
-        @arrayAnt = @notas[0, localizacao]
-        @arrayPos = @notas[localizacao, @notas.size]
-        @newArray = @arrayPos + @arrayAnt
+        localizacao = @notas.find_index(nota)
+        reorganizarArray(localizacao)
 
         temp = 0
         for x in 0...7 do
@@ -41,17 +42,14 @@ class TeoriaMusical
 
         return @escalaMenor
     end
-    
 
-    def iniciar
-        puts "Olá selecione uma nota músical das apresentadas a seguier:\n #{@notas}"
-        notaEscolida = gets
-        
-        puts "A sequência maior é #{notasMaiores(notaEscolida)}"
-        puts "A sequência menor é #{notasMenores(notaEscolida)}" 
+    def reorganizarArray(localizacao)
+        arrayAnt = @notas[0, localizacao]
+        arrayPos = @notas[localizacao, @notas.size]
+        @newArray = arrayPos + arrayAnt
     end
 
 end
 
 teoriaMusical = TeoriaMusical.new
-teoriaMusical.notasMaiores("D#")
+teoriaMusical.iniciar
